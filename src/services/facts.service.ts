24fa0e04cb0ea1c4f9b87a3e8fcd9a67a2ad388e
francisco-lindemayer <francisco.lindemayer@gmail.com>
2021-08-10 03:54:12 -0300

    Change RandomFacts approch to singleton

--- src/services/facts.service.ts
+++ src/services/facts.service.ts
@@ -10,15 +10,19 @@ interface IDogFacts {
   sentCount: number;
 }
 
-export const getRandomDogFacts = async (): Promise<string[]> => {
-  try {
-    const ANIMAL_TYPE = 'dog';
-    const AMOUNT_FACTS = 3;
-    const { data } = await axios.get<IDogFacts[]>(`https://cat-fact.herokuapp.com/facts/random?animal_type=${ANIMAL_TYPE}&amount=${AMOUNT_FACTS}`, { timeout: 1000 });
+class RandomFacts {
+  public async getRandomDogFacts(): Promise<string[]> {
+    try {
+      const ANIMAL_TYPE = 'dog';
+      const AMOUNT_FACTS = 3;
+      const { data } = await axios.get<IDogFacts[]>(`https://cat-fact.herokuapp.com/facts/random?animal_type=${ANIMAL_TYPE}&amount=${AMOUNT_FACTS}`, { timeout: 1000 });
 
-    return data.map(({ text }) => text);
-  } catch (error) {
-    console.error('Falha ao buscar fatos. Fatos obtidos localmente');
-    return ['A impressão do nariz de um cachorro é tão única quanto uma impressão digital humana', 'Os cachorro suam pelas patas', 'Cauda do seu cão é realmente parte de sua espinha'];
+      return data.map(({ text }) => text);
+    } catch (error) {
+      console.error('Falha ao buscar fatos. Fatos obtidos localmente');
+      return ['A impressão do nariz de um cachorro é tão única quanto uma impressão digital humana', 'Os cachorro suam pelas patas', 'Cauda do seu cão é realmente parte de sua espinha'];
+    }
   }
 }
+
+export default new RandomFacts()
