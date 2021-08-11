209b0deb074c6dd0ecb10b9930cdbec3168bb5e8
francisco-lindemayer <francisco.lindemayer@gmail.com>
2021-08-10 23:54:03 -0300

    Remove depricated dependency

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
 import 'dotenv/config'
 import express from 'express'
-import bodyParser from 'body-parser';
 import cors from 'cors';
 import { router } from '@routes/TodoRoute';
 import './database';
 
 const app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use(router);
